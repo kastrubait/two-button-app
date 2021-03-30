e07540764fcdd85aabde8d5223eee6fe288f1551
kastrubait <60570588+kastrubait@users.noreply.github.com>
2021-03-30 08:45:03 +0300

    fix: blocked by CORS policy

--- src/app/base/constansts/constants.ts
+++ src/app/base/constansts/constants.ts
@@ -1,3 +1,3 @@
 export const NUMBER_RETRIES_OF_REQUESTS = 3;
 
-export const BASE_URL = 'http://www.mrsoft.by/data.json';
+export const BASE_URL = 'https://cors-anywhere.herokuapp.com/http://www.mrsoft.by/data.json/';
